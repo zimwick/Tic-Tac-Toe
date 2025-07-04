b590e7acb6e887236fa3158a9b7b5edfbb91b9ee
zimwick <121416169+zimwick@users.noreply.github.com>
2025-07-04 00:11:07 -0600

    Build win logic
    created function for win logic

--- script.js
+++ script.js
@@ -1,15 +1,70 @@
 "use strict";
 
-const gameBoard = {
+const gameBoard = (function () {
+  const setTile = (tile, symbol) => (tiles[tile] = symbol);
+  const getTiles = () => tiles;
+  const clearBoard = function () {
+    tiles.fill("");
+  };
+
   //array position 0 bottom left position 8 top right
-  tiles: ["", "", "", "", "", "", "", "", ""],
-};
+  const tiles = ["", "", "", "", "", "", "", "", ""];
 
-const gameflow = {};
+  return {
+    setTile,
+    getTiles,
+    clearBoard,
+  };
+})();
 
-const playerX = {};
+function createPlayer(name) {
+  return { name };
+}
 
-const playerO = {};
+const gameFlow = (function () {
+  let gameRunning = true;
+  const goesFirst = Math.floor(Math.random() * 2);
+  const player1 = createPlayer("Jay");
+  const player2 = createPlayer("Tayvia");
+
+  if (goesFirst === 0) {
+    //player1 goes first
+  }
+
+  while (gameRunning) {
+    //perform move here first
+
+    //then check winner
+    checkWinner();
+  }
+
+  function checkWinner() {
+    const tile = gameBoard.getTiles();
+
+    const wins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    wins.forEach((win) => {
+      if (
+        tile[win[0]] === tile[win[1]] &&
+        tile[win[1]] === tile[win[2]] &&
+        tile[win[0]] !== "" &&
+        tile[win[1]] !== "" &&
+        tile[win[2]] !== ""
+      ) {
+        gameRunning = false;
+      }
+    });
+  }
+})();
 
 // each player is X or O
 // have a function that randomly decides which player goes first
