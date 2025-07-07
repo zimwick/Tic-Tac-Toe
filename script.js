e495be2b15af9006384a0e74f6f7965e705bdd02
zimwick <121416169+zimwick@users.noreply.github.com>
2025-07-07 16:41:10 -0600

    Implement reset functionality

--- script.js
+++ script.js
@@ -25,11 +25,26 @@ const gameController = (function () {
   let turns = 0;
   let gameRunning = true;
   let playerXTurn = null;
-  let playerX;
-  let playerO;
+  let playerX = "";
+  let playerO = "";
+  let winner = "";
 
   function getPlayerXTurn() {
     return playerXTurn;
+  }
+
+  function getWinner() {
+    return winner;
+  }
+
+  function resetState() {
+    turns = 0;
+    gameRunning = true;
+    playerXTurn = null;
+    playerX = "";
+    playerO = "";
+    winner = "";
+    gameBoard.clearBoard();
   }
 
   function playerSetup(playerXname, playerOname) {
@@ -84,8 +99,10 @@ const gameController = (function () {
       ) {
         if (playerXTurn) {
           console.log(playerX.name, "Wins!");
+          winner = playerX.name;
         } else {
           console.log(playerO.name, "Wins!");
+          winner = playerO.name;
         }
         gameRunning = false;
       }
@@ -93,10 +110,18 @@ const gameController = (function () {
     //check for tie
     if (turns === 8 && gameRunning != false) {
       console.log("its a tie!");
+      winner = "tie";
       gameRunning = false;
     }
   }
-  return { executeTurn, executeTurn, playerSetup, getPlayerXTurn };
+  return {
+    executeTurn,
+    executeTurn,
+    playerSetup,
+    getPlayerXTurn,
+    getWinner,
+    resetState,
+  };
 })();
 
 const displayController = (function () {
@@ -104,14 +129,33 @@ const displayController = (function () {
   const startBtn = document.getElementById("form-submit");
   const playerXname = document.getElementById("playerX");
   const playerOname = document.getElementById("playerO");
+  const formContainer = document.querySelector(".start-form");
+  const turnDisplay = document.querySelector(".turn-score");
+  const resetBtn = document.getElementById("reset");
 
   startBtn.addEventListener("click", function (e) {
     e.preventDefault();
     if (!playerXname.value || !playerOname.value) {
       alert("Please enter 2 player names");
     } else {
+      formContainer.classList.add("hidden");
+      turnDisplay.classList.remove("hidden");
       gameController.playerSetup(playerXname.value, playerOname.value);
+      turnDisplay.textContent = gameController.getPlayerXTurn()
+        ? `${playerXname.value}'s turn`
+        : `${playerOname.value}'s turn`;
     }
+  });
+
+  resetBtn.addEventListener("click", function () {
+    gameController.resetState();
+    turnDisplay.textContent = "";
+    turnDisplay.classList.add("hidden");
+    formContainer.classList.remove("hidden");
+    playerXname.value = "";
+    playerOname.value = "";
+    resetBtn.classList.add("hidden");
+    tiles.forEach((tile) => (tile.textContent = ""));
   });
 
   tiles.forEach((tile) =>
@@ -126,6 +170,19 @@ const displayController = (function () {
         //prevent multiple clicks on same tile
         if (tile.textContent === "") {
           tile.textContent = gameController.executeTurn(tile.id);
+          turnDisplay.textContent = gameController.getPlayerXTurn()
+            ? `${playerXname.value}'s turn`
+            : `${playerOname.value}'s turn`;
+          if (
+            gameController.getWinner() &&
+            gameController.getWinner() != "tie"
+          ) {
+            turnDisplay.textContent = gameController.getWinner() + " Wins!";
+            resetBtn.classList.remove("hidden");
+          } else if (gameController.getWinner() === "tie") {
+            turnDisplay.textContent = "Its a tie!";
+            resetBtn.classList.remove("hidden");
+          }
         }
       }
     })
